b1fdda7999fe2ad2d6ace12a47f55e5b885a027d
aakanksha-02 <wal@Wests-MacBook-Pro-3.local>
2019-07-03 15:53:29 +0530

    unsuccessful attempt of adding redux devtool

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,15 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+// import { composeWithDevTools } from 'redux-devtools-extension';
+
 import reducer from '../reducers';
 
 const initialState = { 
-    name: 'Jenny',
-    currentBalance: '500000'
+  name: 'Jenny',
+  currentBalance: '50000'
 };
+
 export const store = createStore(reducer, initialState);
+//   , composeWithDevTools(
+//   applyMiddleware(),
+// ));
+
